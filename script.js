751f0a1f6393a5d60eea642278c7d6e37af322e2
surajgm-coder <suraj.gm@nkenters.com>
2025-12-18 23:21:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,10 @@ document.addEventListener("DOMContentLoaded", () => {
     showSlide(current);
   }, 5000); // change slide every 5 seconds
 });
+// MOBILE MENU
+const menuBtn = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".main-nav");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
