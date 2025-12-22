bfc49df9f3041c5ba9a331f3ba05676ae0203ce4
surajgm-coder <suraj.gm@nkenters.com>
2025-12-23 00:41:23 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,15 +1,19 @@
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll('.nk-slide');
 const dots = document.querySelectorAll('.dot');
 let index = 0;
 
 function showSlide(i) {
   slides.forEach(s => s.classList.remove('active'));
   dots.forEach(d => d.classList.remove('active'));
+
   slides[i].classList.add('active');
   dots[i].classList.add('active');
+
+  index = i;
 }
 
+/* AUTO SLIDE */
 setInterval(() => {
   index = (index + 1) % slides.length;
   showSlide(index);
-}, 5000);
+}, 6000); // comfortable premium timing
