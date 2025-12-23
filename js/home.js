311655e14b5294c6f945e6361381286b6db3196d
surajgm-coder <suraj.gm@nkenters.com>
2025-12-23 16:13:44 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,17 +1,1 @@
-<script>
-document.addEventListener("DOMContentLoaded", () => {
-  const slides = document.querySelectorAll(".nk-slide");
-  const dots = document.querySelectorAll(".dot");
-  let index = 0;
 
-  setInterval(() => {
-    slides[index].classList.remove("active");
-    dots[index].classList.remove("active");
-
-    index = (index + 1) % slides.length;
-
-    slides[index].classList.add("active");
-    dots[index].classList.add("active");
-  }, 5000); // 5 seconds
-});
-</script>
